1672916c15ab28c02d96e59e0e80460159f25005
Gris Ge <gef5@chinaunicom.cn>
2026-06-28 17:51:24 +0800

    Add integration test for linux bridge vlan config
    Verify updating bridge and per-port VLAN mode/tags/trunk ranges is
    reflected in the queried state.
    
    Signed-off-by: Gris Ge <gef5@chinaunicom.cn>

--- tests/linux_bridge_test.py
+++ tests/linux_bridge_test.py
@@ -210,3 +210,63 @@ def test_modify_linux_bridge_port_options(linux_bridge_over_dummy):
     )
 
 
+def test_modify_linux_bridge_vlan(linux_bridge_over_dummy):
+    nipart.apply(load_yaml(f"""---
+            interfaces:
+              - name: {TEST_BRIDGE_NIC}
+                type: linux-bridge
+                bridge:
+                  vlan:
+                    mode: access
+                    tag: 301
+                  ports:
+                    - name: {TEST_PORT2}
+                      vlan:
+                        mode: access
+                        tag: 701
+                    - name: {TEST_PORT1}
+                      vlan:
+                        mode: trunk
+                        trunk-tags:
+                        - id: 401
+                        - id-range:
+                            min: 501
+                            max: 601
+            """))
+    linux_bridge_iface = show_only(TEST_BRIDGE_NIC)
+    assert state_match(
+        [
+            {
+                "name": TEST_PORT1,
+                "vlan": {
+                    "mode": "trunk",
+                    "trunk-tags": [
+                        {
+                            "id": 401,
+                        },
+                        {
+                            "id-range": {
+                                "min": 501,
+                                "max": 601,
+                            }
+                        },
+                    ],
+                },
+            },
+            {
+                "name": TEST_PORT2,
+                "vlan": {
+                    "mode": "access",
+                    "tag": 701,
+                },
+            },
+        ],
+        linux_bridge_iface["bridge"]["ports"],
+    )
+    assert state_match(
+        {
+            "mode": "access",
+            "tag": 301,
+        },
+        linux_bridge_iface["bridge"]["vlan"],
+    )
