2befe2f2b7e62a0de496afecfaba7abcbe3bd1ca
Gris Ge <gef5@chinaunicom.cn>
2026-08-03 18:46:20 +0800

    Add unit test for `is_option_string_empty` on `None`
    Verify `is_option_string_empty` treats `None` as empty.
    
    Signed-off-by: Gris Ge <gef5@chinaunicom.cn>

---
+++ src/lib/schema/unit_tests/serializer.rs
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache-2.0
+
+use serde::Serialize;
+
+use crate::serializer::{is_option_string_empty, option_u32_as_hex};
+
+#[derive(Serialize)]
+struct TestHexU32 {
+    #[serde(serialize_with = "option_u32_as_hex")]
+    v: Option<u32>,
+}
+
+#[test]
+fn test_serialize_is_option_string_empty_none() {
+    assert!(is_option_string_empty(&None));
+}
+
